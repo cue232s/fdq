31cf1e1903ca25ab1fb4cc914f8aee3480959e1c
Edu Ubuntu <eduerondu@gmail.com>
2012-06-04 14:16:15 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fdq::Application.config.secret_token = '00ebf0577227f6ac4d2c3fbbed43edf1aa830b030cf2571564546b4ed0bbfa3e98098d33e872da36a523a75639fd857f250711563c062bc9311fc3dfe79bdf7c'
