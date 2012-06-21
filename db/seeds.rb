742feba75d758905556c40b0f7498d94d93a412c
Edu Ubuntu <eduerondu@gmail.com>
2012-06-21 14:29:38 -0500

    seeds for SearchTerm model, and added search terms to home/indes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Terms = SearchTerm.create([{name: 'Pizza'}, {name: 'Chicken'}, {name: 'Burgers'}, {name: 'Chinese'}, {name: 'Thai'}])
--- test/unit/search_term_test.rb
+++ test/unit/search_term_test.rb
@@ -4,4 +4,10 @@ class SearchTermTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should always require a name" do
+  	term = SearchTerm.new
+  	assert !term.save
+  end
+
+  
 end
