e7869239c267a06864262abd8b74140fa64d30ce
Edu Ubuntu <eduerondu@gmail.com>
2012-06-26 15:26:29 -0500

    add tags display to home restaurant show page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,14 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :description, :name, :phone, :state, :street_address, :street_address_2, :zipcode
+  attr_accessible :description, :name, :phone, :state, :street_address, :street_address_2, :zipcode, :tag_names
 
   has_many :taggings, :dependent => :destroy
   has_many :tags, :through => :taggings
 
   attr_accessor :tag_names
+
+  def tag_names
+    @tag_names || tags.map(&:name).join(', ')
+  end
 
   #accepts_nested_attributes_for :tags, :allow_destroy => :true, 
   #	:reject_if => proc { |attrs| attrs.all? { |k, v| v.blank?}}
@@ -13,16 +17,13 @@ after_save :assign_tags
 
 
 
-private
+  private
 
-
-def assign_tags
-	if @tag_names
-		self.tags = @tags_names.split(/\s+/).map do |name|
-			Tag.find_or_create_by_name(name)
+  def assign_tags
+  	if @tag_names
+  		self.tags = @tag_names.split( /, */ ).map do |name|
+  			Tag.find_or_create_by_name(name)
+      end
     end
   end
-
-end
-
 end
